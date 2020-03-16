95826ea8be42e060baece68b715fc5468f686b52
Luis Gerardo <gerardwjones@gmail.com>
2020-03-15 22:39:39 -0600

    Draw in canvas like a pencil wit a draw function

---
+++ src/scripts/script.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(window.innerWidth, window.innerHeight);
+
+}
+
+function draw(){
+
+
+
+    /* Draw in the canvas like a pencil
+    if(mouseIsPressed){
+        fill(0);
+        circle(mouseX, mouseY, 5);
+    }else{
+        fill(100);
+    } */
+
+}
